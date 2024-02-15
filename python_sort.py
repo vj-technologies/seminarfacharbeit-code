579313dfbb92d5ce810097e4becf42d88a9155bf
vinzenz-fischer <vhfischer@outlook.de>
2024-02-15 23:27:39 +0100

    Update python_sort.py

--- python_sort.py
+++ python_sort.py
@@ -1,36 +1,33 @@
-import random,time,threading
+import random, time, threading
 runtimes = []
 
-lol =[]
+lol = []
 
 def gen_ran_list(lens = 1000, maxvalue = 10000):
     return_list = []
     for i in range(lens):
-        return_list.append(random.randint(0,maxvalue))
+        return_list.append(random.randint(0, maxvalue))
     return return_list
 
-def buubble_sort(In:list[int]):
-    for _ in range(len(In)-1):
-        for i in range (len(In)-1):
-            if In[i] >= In[i+1]:
-                In[i],In[i+1] = In[i+1] , In[i]
+def bubble_sort(array: list[int]):
+    for _ in range(len(array)-1):
+        for i in range(len(array)-1):
+            if array[i] >= array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
 
-    
     runtimes.append(time.thread_time())
-  
+
 
 for i in range(100):
     lol.append(gen_ran_list())
 
 for k in range(len(lol)):
-    t = threading.Thread(target=buubble_sort, args=[lol[k]])
+    t = threading.Thread(target=bubble_sort, args=[lol[k]])
     t.start()
     t.join()
 
 
-
 avg_run = sum(runtimes)/len(runtimes)
-
 
 
 print(f"the average runtime is: {avg_run} seconds")
