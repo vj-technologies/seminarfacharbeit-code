516498ff537ceec23808f7708f3479eb2e38a8ce
Jannisboensch <107045303+Jannisboensch@users.noreply.github.com>
2024-02-17 17:23:59 +0100

    Add files via upload

--- python_sort.py
+++ python_sort.py
@@ -1,19 +1,31 @@
-import random, time, threading
+import random,time,threading
 runtimes = []
 
-lol = []
+lol =[]
 
-def gen_ran_list(lens = 1000, maxvalue = 10000):
+def gen_ran_list(lens = 1000, maxvalue = 1000):
     return_list = []
     for i in range(lens):
-        return_list.append(random.randint(0, maxvalue))
+        return_list.append(random.randint(0,maxvalue))
     return return_list
 
-def bubble_sort(array: list[int]):
-    for _ in range(len(array)-1):
-        for i in range(len(array)-1):
-            if array[i] >= array[i+1]:
-                array[i], array[i+1] = array[i+1], array[i]
+def bubble_sort(array:list[int]):
+    for _ in range(len(array)-1): #
+        for N in range (len(array)-1): #
+            if array[N] >= array[N+1]: # 
+                array[N],array[N+1] = array[N+1] , array[N]
+
+    
+    runtimes.append(time.thread_time())
+
+def selection_sort(array:list[int]):
+    for i in range(len(array)):
+        jmin = i
+        for j in range(i+1, len(array)):
+            if array[j] < array[jmin]:
+                jmin = j
+        array[i],array[jmin] = array[jmin],array[i]
+
 
     runtimes.append(time.thread_time())
 
@@ -22,12 +34,14 @@ for i in range(100):
     lol.append(gen_ran_list())
 
 for k in range(len(lol)):
-    t = threading.Thread(target=bubble_sort, args=[lol[k]])
+    t = threading.Thread(target=selection_sort, args=[lol[k]])
     t.start()
     t.join()
 
 
+
 avg_run = sum(runtimes)/len(runtimes)
+
 
 
 print(f"the average runtime is: {avg_run} seconds")
